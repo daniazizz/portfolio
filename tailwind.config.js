c1f718f76c9d7c8ef00269466c5e6bb44ceefa78
Dani Aziz <dany1002008@gmail.com>
2023-04-04 18:40:34 +0200

    Extended tailwind with custom cursor classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      cursor: {
+        default: "url(src/assets/svgs/cursor-default.svg), default",
+        pointer: "url(src/assets/svgs/cursor-pointer.svg), pointer",
+      },
       fontFamily: {
-        matrix:['matrix']
-      }
+        matrix: ["matrix"],
+      },
     },
   },
   plugins: [],
-}
+};
